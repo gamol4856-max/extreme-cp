27637591acde1cea01a14de75df2aaf2594e9099
gamol4856-max <gamol4856@gmail.com>
2026-03-11 19:50:17 +0530

    fix static files

--- extreme_site/settings.py
+++ extreme_site/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
